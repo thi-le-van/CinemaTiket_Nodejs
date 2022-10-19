0a955168ddb08d5847415cc8b799a29ba81f95f2
I Karos <aotamez1@gmail.com>
2022-10-19 23:45:24 +0700

    fix something

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'https://xemphim-clone-oh5fprerl-levanthi.vercel.app/',
+    origin: 'https://xemphim-clone.vercel.app',
     // origin: 'http://localhost:3000',
     credentials: true,
   })
